014f4ec5a6a260caa1adfb0ea9cf359e93c31f18
PeterHorat <peterhorat@hispeed.ch>
2018-02-05 15:18:33 +0100

    Verbesserungen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import logo from './sleeping_dog_blue.GIF';
 import './App.css';
 import FriendsContainer from './FriendsContainer.js';
 
@@ -37,7 +37,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React, Peter</h1>
+                    <h1 className="App-title">Sleeping Dog</h1>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
