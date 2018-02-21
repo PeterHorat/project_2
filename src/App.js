fa3750d582f13d82351bf3455a3fea0e871c613f
PeterHorat <peterhorat@hispeed.ch>
2018-02-21 18:10:12 +0100

    MyMusicpage bootstrap_theme_band.asp

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import SturmTags from './SturmTags.js';
 import Friends from './Friends.js';
 import News from './News.js';
 import {Grid, Row, Col} from 'react-flexbox-grid';
-import { Player } from 'video-react';
+import {Player} from 'video-react';
 import "./video-react.css";
+import { Carousel } from 'react-bootstrap';
+import toffee from './img/toffee_token.jpg';
+import toffee_sw from './img/toffee_sw.jpg';
+import geiss from './img/geiss_sw.jpg';
+import wald from './img/wald_sw.jpg';
 
 import {
     BrowserRouter as Router,
@@ -62,53 +67,105 @@ const Topics = ({match}) => (
 
 
 class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleSelect = this.handleSelect.bind(this);
+
+        this.state = {
+            index: 0,
+            direction: null
+        };
+    }
+
+    handleSelect(selectedIndex, e) {
+        alert(`selected=${selectedIndex}, direction=${e.direction}`);
+        this.setState({
+            index: selectedIndex,
+            direction: e.direction
+        });
+    }
 
     render() {
+        const { index, direction } = this.state;
         return (
-           <div className="flex-container">
-                <header>
-
-                    <Grid fluid>
-                        <Row>
-                            <Col xs={12} md={12}>
-                                <img src={logo} className="App-logo" alt="logo"/>
-                            </Col>
-                            <Col xs={12} md={12}>
-                                <h1 className="text-info">Sleeping Dog</h1>
-                            </Col>
-                        </Row>
-                    </Grid>
-                </header>
-
-
+            <div>
+                <head>
+                    <title>Sleeping Dog</title>
+                </head>
                 <body>
+                <Carousel activeIndex={index}
+                          direction={direction}
+                          onSelect={this.handleSelect}>
+                    <Carousel.Item>
+                        <img width={1200} height={900} alt="1200x900" src={toffee_sw} />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={1200} height={900} alt="1200x900" src={geiss}/>
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={1200} height={900} alt="1200x900" src={wald}/>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>;
 
-                <Router>
-                    <div>
-                        <ul className="navigation">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/sturmtags">SturmTags</Link></li>
-                            <li><Link to="/friends">Friends</Link></li>
-                            <li><Link to="/news">News</Link></li>
-                            <li><Link to="/topics">Topics</Link></li>
-                        </ul>
 
-                        <hr/>
-
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/sturmtags" component={SturmTags}/>
-                        <Route path="/friends" component={Friends}/>
-                        <Route path="/news" component={News}/>
-                        <Route path="/topics" component={Topics}/>
-                    </div>
-                </Router>
-
-
+                <div className="container text-center">
+                    <h3>Sleeping Dog</h3>
+                    <p>We love music!</p>
+                    <p>We have created a fictional band website. Lorem ipsum..</p>
+                    <Router>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <p><strong>Toffee</strong></p><br/>
+                                <Link to='#demo' data-toggle="collapse">
+                                    <img className='img-circle person' src={toffee} alt="Random Name"/>
+                                </Link>
+                                <div id='demo'>
+                                    <p>Guitarist and Lead Vocalist</p>
+                                    <p>Loves long walks on the beach</p>
+                                    <p>Member since 1988</p>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <p><strong>Toffee</strong></p><br/>
+                                <Link to='#demo2' data-toggle="collapse">
+                                    <img className='img-circle person' src={toffee} alt="Random Name"/>
+                                </Link>
+                                <div id="demo2">
+                                    <p>Drummer</p>
+                                    <p>Loves drummin'</p>
+                                    <p>Member since 1988</p>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <p><strong>Toffee</strong></p><br/>
+                                <Link to='#demo3' data-toggle="collapse">
+                                    <img className='img-circle person' src={toffee} alt="Random Name"/>
+                                </Link>
+                                <div id="demo3">
+                                    <p>Bass player</p>
+                                    <p>Loves math</p>
+                                    <p>Member since 2005</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Router>
+                </div>
                 </body>
-                <footer>footer</footer>
-
             </div>
-        );
+        )
     }
 }
 
