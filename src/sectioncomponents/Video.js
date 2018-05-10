8cf5ba0f5f65a3e10fd545269ab3c96f9d53553e
PeterHorat <peterhorat@hispeed.ch>
2018-05-10 16:54:14 +0200

    mehrere Videos

--- src/sectioncomponents/Video.js
+++ src/sectioncomponents/Video.js
@@ -7,24 +7,26 @@ class Video extends Component {
 
     render() {
         return (
-            <div id="video" className="video_container container">
-                <div className="row text-center">
-                    <div className="col-sm-6">
-                        <div className="player-wrapper">
-                            <ReactPlayer className='react-player'
-                                         url='https://www.dropbox.com/s/3ughs6fzfrjqvoy/Video%2007.05.18%2C%2017%2057%2025.mp4?dl=0'
-                                         width='100%'
-                                         height='100%'
-                                         playing/>
+            <div id="video" className="video-wrapper container">
+                <div className="video_container">
+                    <div className="row text-center">
+                        <div className="col-sm-6">
+                            <div className="player-wrapper">
+                                <ReactPlayer className='react-player'
+                                             url='https://www.dropbox.com/s/3ughs6fzfrjqvoy/Video%2007.05.18%2C%2017%2057%2025.mp4?dl=0'
+                                             width='100%'
+                                             height='100%'
+                                             playing/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="player-wrapper">
-                            <ReactPlayer className='react-player'
-                                         url='https://www.dropbox.com/s/3ughs6fzfrjqvoy/Video%2007.05.18%2C%2017%2057%2025.mp4?dl=0'
-                                         width='100%'
-                                         height='100%'
-                                         playing/>
+                        <div className="col-sm-6">
+                            <div className="player-wrapper">
+                                <ReactPlayer className='react-player'
+                                             url='https://www.dropbox.com/s/3ughs6fzfrjqvoy/Video%2007.05.18%2C%2017%2057%2025.mp4?dl=0'
+                                             width='100%'
+                                             height='100%'
+                                             playing/>
+                            </div>
                         </div>
                     </div>
                 </div>
